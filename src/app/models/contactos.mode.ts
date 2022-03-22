009c512fcfaa9bdd0ae953415acd4dd3992809c3
iFreuk <luiguillog2@gmail.com>
2022-03-21 22:35:27 -0600

    [Add] Contactos
    [Added] Contactos serives
    [Added] Contactos model

---
+++ src/app/models/contactos.mode.ts
@@ -0,0 +1,7 @@
+export interface contacto {
+    apellido_1 : string;
+    apellido_2 : string;
+    correo : string;
+    nombre : string
+    telefono : string
+}
--- src/app/services/contactos.service.ts
+++ src/app/services/contactos.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {AngularFirestore} from "@angular/fire/compat/firestore";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactosService {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
+
+  getContactos(){
+    const contactos = this.db.collection('contactos').valueChanges();
+    return contactos;
+  }
+
 }

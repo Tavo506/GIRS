12275208641349d752be64a346c421e985a6e094
iFreuk <luiguillog2@gmail.com>
2022-03-21 18:04:14 -0600

    NewMunicipalidadModel
    Added Municipalidad data Model

---
+++ src/app/models/municipalidad.model.ts
@@ -0,0 +1,17 @@
+export interface Municipalidad{
+    anno: number;
+    canto: string;
+    formaDisposicion: string;
+    gastoServicioRecoleccionResiduos: number;
+    gastosServicioAseoDeVias:number;
+    ingresoServicioRecoleccionRSM:number;
+    lugarDisposicion: string;
+    perosnasHabitacion: number;
+    ppc: number;
+    programaDeRecliclaje: string;
+    provinicia: string;
+    recoleccionDeResiduosSolidosValorizables: string;
+    totalIngresoServicioAseoDeVias: number;
+    unidadesHabitacionalesAtendidas: number;
+    unidadesHabitacioanlesCanton: number;
+}

5425e38994190b96a8c493c8857c1c6987abc8fa
AgusBA011 <agustinblanco01@gmail.com>
2022-04-14 19:49:34 -0600

    [Update] - Design ready

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -12,4 +12,23 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  //Función que realiza el inicio de sesión
+
+
+
+
 }
+
+
+/*Animations
+
+https://jasonwatmore.com/post/2019/11/04/angular-8-router-animation-tutorial-example
+
+Referencias LogIn
+
+https://github.com/Tavo506/Comunidad-Intro-Taller/blob/main/src/app/pages/login/login.component.ts
+
+
+
+*/
